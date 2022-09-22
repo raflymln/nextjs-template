f09a99d0c79f064b15b5d6667dec8ab855080745
Rafly Maulana <mraflymaulana@gmail.com>
2022-09-22 13:13:02 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
-/**
- * TailwindCSS Configuration
- * @type {import('tailwindcss/tailwind-config').TailwindConfig}
- */
 module.exports = {
     mode: "jit",
-    purge: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
-    darkMode: false, // or 'media' or 'class'
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx}", //
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
+        corePlugins: {
+            container: false,
+        },
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+        },
+        fontFamily: {},
         extend: {},
     },
     variants: {
-        extend: {},
+        extend: {
+            screens: {
+                "3xl": "1920px",
+            },
+        },
     },
     plugins: [],
 };
