c1ce0ae0a0251465bf69f12f9601c8f21ec04e68
Rafly Maulana <mraflymaulana@gmail.com>
2023-06-20 03:10:23 +0700

    Update configuration

--- next.config.js
+++ next.config.js
@@ -1,39 +1,9 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+// @ts-check
 
-const config = (phase) => {
-    /**
-     * Plugins
-     */
-    let plugins = [];
-
-    /**
-     * Configuration
-     * @type {import('next').NextConfig}
-     */
-    let settings = {
-        reactStrictMode: true,
-        poweredByHeader: false,
-        cleanDistDir: true,
-        redirects: async () => {
-            return [];
-        },
-        rewrites: async () => {
-            return [];
-        },
-    };
-
-    if (phase !== PHASE_DEVELOPMENT_SERVER) {
-        // do something in production mode
-    }
-
-    return {
-        plugins,
-        settings,
-    };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    poweredByHeader: false,
 };
 
-const pipe = (funcs) => (value) => funcs.reduce((v, f) => f(v), value);
-module.exports = (phase, { defaultConfig }) => {
-    const cfg = config(phase, { defaultConfig });
-    return pipe(cfg.plugins)(cfg.settings);
-};
+module.exports = nextConfig;
