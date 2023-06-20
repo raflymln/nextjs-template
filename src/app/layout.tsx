07ab1c51b9e6698293f2cf7aefb9c480679aa18f
Rafly Maulana <mraflymaulana@gmail.com>
2023-06-20 15:52:42 +0700

    Update examples

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
-import "@/styles/globals.css";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <head />
-            <body className="bg-black text-white">{children}</body>
+            <body>{children}</body>
         </html>
     );
 }
