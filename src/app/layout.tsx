53e8c6432f91d6ef6faab24fb19a649b8e047f35
Rafly Maulana <mraflymaulana@gmail.com>
2023-06-20 16:17:18 +0700

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "My Next.js App",
+    description: "Welcome to my Next.js app!",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
